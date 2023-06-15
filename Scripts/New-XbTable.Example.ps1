402742909da9f299aa8fba49c5d821c447063fbb
PeterVandivier <peter.vandivier@gmail.com>
2023-06-15 13:46:37 -0400

    🧑‍💻 example usage `New-XbTable`

---
+++ Scripts/New-XbTable.Example.ps1
@@ -0,0 +1,12 @@
+
+$newTableSplat = @{
+    ResourceGroupName = 'my-resource-group'
+    StorageAccountName = 'mystorageaccount'
+    Container = 'storm-events'
+    ClusterUrl = 'https://help.kusto.windows.net;Fed=True'
+    DatabaseName = 'Samples'
+    TableName = 'StormEvents'
+    TimestampColumnName = 'StartTime'
+}
+
+New-XbTable @newTableSplat -ErrorAction SilentlyContinue
