e8f15424ce9440087554a63a1d29b842418307d5
PeterVandivier <peter.vandivier@gmail.com>
2023-06-30 13:29:36 -0400

    ✅ tests non-contiguous batch `Export-Tbl` support

--- Tests/Export-XbTable.Tests.ps1
+++ Tests/Export-XbTable.Tests.ps1
@@ -1,14 +1,26 @@
+#Requires -Modules @{ModuleName='Pester';MaximumVersion='4.99';Guid='a699dea5-2c73-4616-a270-1f7abb777e71'}
 
 Describe "Planned steps should remain consistent between " {
-    Push-Location $PsScriptRoot
-
     $caseName = "Export-XbTable.1"
-    $case1 = Get-Content "Configuration/${caseName}.json" | ConvertFrom-Json -AsHashtable
-    Export-XbTable @case1 -Verbose -NoExecute 4>&1 | Set-Content "Output/${caseName}.txt"
-    $output = Get-Content "Output/${caseName}.txt" -Raw
-    $control = Get-Content "Control/${caseName}.txt" -Raw
+    $case = Get-Content "$PsScriptRoot/Configuration/${caseName}.json" | ConvertFrom-Json -AsHashtable
+    Export-XbTable @case -Verbose -NoExecute 4>&1 | Set-Content "$PsScriptRoot/Output/${caseName}.txt"
+    $output = Get-Content "$PsScriptRoot/Output/${caseName}.txt" -Raw
+    $control = Get-Content "$PsScriptRoot/Control/${caseName}.txt" -Raw
 
     It "Case 1: calendar year 2007, 1 day (24 hour) step size, DOP 31. " {
+        $output | Should -BeExactly $control
+    }
+
+    $caseName = "Export-XbTable.2"
+    $case = Get-Content "$PsScriptRoot/Configuration/${caseName}.json" | ConvertFrom-Json -AsHashtable
+    $bounds =  New-XbBatchBounds -Start '2007-01-01' -End '2007-02-01' -Step '1.00:00'
+    $bounds += New-XbBatchBounds -Start '2007-03-01' -End '2007-04-01' -Step '7.00:00'
+    $case.Add('BatchBounds',$bounds)
+    Export-XbTable @case -Verbose -NoExecute 4>&1 | Set-Content "$PsScriptRoot/Output/${caseName}.txt"
+    $output = Get-Content "$PsScriptRoot/Output/${caseName}.txt" -Raw
+    $control = Get-Content "$PsScriptRoot/Control/${caseName}.txt" -Raw
+
+    It "Case 2: Non-contiguous batch bounds, CY 2007, January (by day) & March (by week), DOP 4. " {
         $output | Should -BeExactly $control
     }
 
