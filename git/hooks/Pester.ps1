9e0f9f2825dffcee5142fcb7fcb8af8c880cf2a8
PeterVandivier <peter.vandivier@gmail.com>
2023-07-13 08:58:38 -0400

    💡 note delay for pre-commit Pester

--- git/hooks/Pester.ps1
+++ git/hooks/Pester.ps1
@@ -1,4 +1,12 @@
 
+<#
+.Synopsis
+    Runs Pester tests on current build.
+.Description
+    Force reload PsAdxArchiver, force Pester v4, runs the tests.
+    Adds about 10 second delay to commit :\
+#>
+
 Import-Module $PsScriptRoot/../../PsAdxArchiver.psd1 -Force
 
 Remove-Module Pester -ErrorAction SilentlyContinue
